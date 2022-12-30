a22e4d0834cb5f622a7cedf348944d50221f7de7
Vladimir Park <101277074+NewiPacK@users.noreply.github.com>
2022-12-30 18:28:20 +0500

    Создал Регистрацию и Авторизацию

---
+++ views/theme/pages/auth.php
@@ -0,0 +1,8 @@
+
+
+
+<form method="POST" action="">
+    <input type="login" name="login" placeholder="Логин" />
+    <input type="password" name="password" placeholder="Пароль" />
+    <input type="submit" value="Авторизоваться"/>
+</form>
---
+++ views/theme/pages/reg.php
@@ -0,0 +1,9 @@
+
+<form method="POST" action="">
+    <input type="text" name="login" placeholder="Логин" /> <br>
+	<input type="password" name="password" placeholder="Пароль" /> <br>
+	<input type="email" name="email" placeholder="Почта" /> <br>
+	<input type="text" name="name" placeholder="Имя" /> <br>
+	<input type="text" name="phone" placeholder="Номер телефона" /> <br>
+	<input type="submit" value="Зарегестрировать" />
+</form>
